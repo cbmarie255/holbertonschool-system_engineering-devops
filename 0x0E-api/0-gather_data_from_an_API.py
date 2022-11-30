181a02c9d599ce49b73bbf31cf40dbf77a01fdc2
cbmarie255 <court.b1993@gmail.com>
2022-11-30 11:22:30 -0800

    Fixed code to be pep8 complaint

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -7,16 +7,17 @@ import json
 import requests
 
 
+API_URL = 'https://jsonplaceholder.typicode.com/'
 # Grab the employee!
 employee_id = argv[1]
-url = 'https://jsonplaceholder.typicode.com/users?id{}='.format(employee_id)
+url = API_URL + 'users?id{}='.format(employee_id)
 response_for_user = requests.get(url)
 employee_data = response_for_user.text
 company_profile = json.loads(employee_data)
 name = company_profile[0].get('name')
 
 # Grab the todo list!
-list_url = 'https://jsonplaceholder.typicode.com/todos?users?Id{}='.format(employee_id)
+list_url = API_URL + 'todos?users?Id{}='.format(employee_id)
 resonse_for_todos = requests.get(list_url)
 to_do_list = resonse_for_todos.text
 tasks = json.loads(to_do_list)
@@ -28,6 +29,8 @@ for task in tasks:
         completed_tasks_count += 1
 
 # Print the output on command line.
-print("Employee {} is done with tasks({}/{}): ".format(name, completed_tasks_count, len(tasks)))
+prompt_1 = "Employee {} is done with ".format(name)
+prompt_2 = "tasks({}/{}):".format(completed_tasks_count, len(tasks))
+print(prompt_1 + prompt_2)
 for task in completed_tasks:
     print("\t {}".format(task.get('title')))
